34cefc95a0cbc8d6fc4363d14a47a5316c125dda
Adnane Guennach <aguennac@c1r5p14.1337.ma>
2025-10-20 14:30:23 +0100

    3tetege

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,45 +6,37 @@
 /*   By: aguennac <aguennac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 14:11:32 by aguennac          #+#    #+#             */
-/*   Updated: 2025/10/20 14:11:51 by aguennac         ###   ########.fr       */
+/*   Updated: 2025/10/20 14:30:03 by aguennac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+#include "libft.h"
 
+int	ft_atoi(const char *str)
+{
+	int					i;
+	int					sign;
+	unsigned long int	result;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (ft_isdigit(str[i]))
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
